ba53932e414a1beb029a7985237facf70d5a536d
steve <truckersteve781@gmail.com>
2022-12-31 00:34:19 -0500

    completed task 4 added multi select test

--- src/test/java/org/TestPractices/Pages/lambdatest/DropDownPage.java
+++ src/test/java/org/TestPractices/Pages/lambdatest/DropDownPage.java
@@ -1,13 +1,11 @@
 package org.TestPractices.Pages.lambdatest;
 
-import org.TestPractices.utilities.Helper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import java.util.List;
 
-import static org.TestPractices.utilities.Helper.refreshPage;
 
 public class DropDownPage {
 
